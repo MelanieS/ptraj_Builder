ecb03b5185e387ff88855f9af55a7de2a279fcf2
Melanie <warchildbosnia@gmail.com>
2014-06-27 02:33:28 -0400

    init

---
+++ step_time.rb
@@ -0,0 +1,71 @@
+#ruby 2.0.0p451 (2014-02-24 revision 45167)
+#energies files must contain time on left column, energy on right, separated by space
+#change paths on lines 5, 51-60, 69
+
+text = File.open('wy_12_60_dyn_energies.txt').read
+text = text.to_s
+array = text.split("\n")
+
+#creates time_arr which only holds time (same indices as main array)
+$time_arr =[]
+
+array.each do |i|
+  white = i.index(" ")
+  white = white.to_i
+  time_num = white - 1
+  time_num = time_num.to_i
+  time = i[0..time_num]
+  $time_arr.push(time)
+end
+
+#puts blank space before and after time to clean up "grep"
+$time_arr.map! {|x| " " + x + " "}
+$cleaning_time = $time_arr
+
+$grep_arr = []
+
+def file_checker(file_path)
+    $grep_arr.push(file_path)
+  puts " "
+  count = 1
+  $time_arr.each do |i|
+    size = $time_arr.size
+    puts "Grepping time #{count} of #{size} in #{file_path}"
+    grepped = File.open(file_path).read
+    grepped = grepped.to_s
+    grepped_array = grepped.split("\n")
+    count = count + 1
+    
+    grepped_array.each do |e|
+      if e.include? i
+        length = e.length
+        clean_str = e[0..43]
+        $grep_arr.push(clean_str)
+      end
+    end
+  end
+  return $grep_arr
+end
+
+results = file_checker('files/WY_12_60_dyn1.out')
+results = file_checker('files/WY_12_60_dyn2.out')
+results = file_checker('files/WY_12_60_dyn3.out')
+results = file_checker('files/WY_12_60_dyn4.out')
+results = file_checker('files/WY_12_60_dyn5.out')
+results = file_checker('files/WY_12_60_dyn6.out')
+results = file_checker('files/WY_12_60_dyn7.out')
+results = file_checker('files/WY_12_60_dyn8.out')
+results = file_checker('files/WY_12_60_dyn9.out')
+results = file_checker('files/WY_12_60_dyn10.out')
+
+def answer()
+  $grep_arr.each do |b|
+  end
+end
+
+puts answer
+
+#writes to new file
+new = File.new("time_step_wy_12_60_dyn.txt", "w")
+new.puts answer
+new.close
